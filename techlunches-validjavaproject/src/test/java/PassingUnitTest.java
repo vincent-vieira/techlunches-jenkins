41937c1595b097cce676df256f59512c980b4cbd
Vincent Vieira <vincent.vieira@supinfo.com>
2016-03-09 14:52:40 +0100

    feat(java): example projects with passing and non-passing unit tests

---
+++ techlunches-validjavaproject/src/test/java/PassingUnitTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Basic unit test passing all assertions.
+ *
+ * @author <a href="mailto:vincent.vieira@supinfo.com">Vincent Vieira</a>
+ */
+public class PassingUnitTest {
+
+    @Test
+    public void passingTest() throws Exception {
+        Assert.assertEquals(1, 1);
+    }
+}
